7ce7b5df0030dde076f7a452acad32ee57a04f27
Anthony Alvarez Delgado <107961797+aalvarez-d@users.noreply.github.com>
2023-03-26 22:37:53 -0600

    required chamges to work

--- DarksidersSurvivors/src/model/Player.java
+++ DarksidersSurvivors/src/model/Player.java
@@ -1,153 +1,153 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package model;
+
+import static model.Constants.PlayerConstants.*;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javax.imageio.ImageIO;
-import static model.Constants.Directions.*;
-import static model.Constants.PlayerConstants.*;
-import view.GamePanel;
 
-/**
- *
- * Program created by
- * @author Anthony Alvarez Delgado
- * Software Engeneer Student - UIA
- *
- */
-public class Player extends Character {
-    private BufferedImage[][] animations;
-    private int animationTick;
-    private int animationIndex;
-    private int animationSpeed;
-    private int playerAction;
-    private boolean left, up, right, down;
-    private boolean moving;
-    private float playerSpeed;
+public class Player extends Entity {
+	private BufferedImage[][] animations;
+	private int aniTick, aniIndex, aniSpeed = 25;
+	private int playerAction = IDLE;
+	private boolean moving = false, attacking = false;
+	private boolean left, up, right, down;
+	private float playerSpeed = 2.0f;
 
-    public Player(float x, float y) {
-        super(x, y);
-        loadAnimations();
-        this.moving = false;
-        this.playerAction = IDLE;
-        this.playerSpeed = 2.0f;
-        this.animationIndex = this.animationSpeed = this.animationTick = 30;
-    }
-    
-    public void update(){
-       
-        updateAnimationTick();
-        updatePosition();
-        setAnimation();
-        
-    }
-    
-    public void render(Graphics g){
-        g.drawImage(animations[playerAction][animationIndex], (int)x, (int) y,120, 120,  null);
-    }
-    
-    private void updateAnimationTick() {
-        this.animationTick++;
-        if(this.animationTick >= this.animationSpeed){
-            this.animationTick = 0;
-            this.animationIndex++;
-            if (this.animationIndex >= GetSpriteAmount(playerAction))
-                this.animationIndex = 0;
-        }
-    }
-    
-    private void setAnimation() {
-        if(moving)
-            playerAction = WALK;
-        else
-            playerAction = IDLE;
-    }
-    
-    private void updatePosition() {
-        moving = false;
-        
-        if(left && !right){
-            x-=playerSpeed;
-            moving = true;
-        } else if (right && !left){
-            x+=playerSpeed;
-            moving = true;
-        }
-        
-        if(up && !down){
-            y-= playerSpeed;
-            moving = true;
-        } else if(down && !up){
-            y=+playerSpeed;
-            moving = true;
-        }
-    
-    }
+	public Player(float x, float y) {
+		super(x, y);
+		loadAnimations();
+	}
 
-    
-    private void loadAnimations() {
-        InputStream is = getClass().getResourceAsStream("/SpriteSheet.png");
-        try {
-            BufferedImage sprite = ImageIO.read(is);
-            
-            animations = new BufferedImage[6][11];
-                for(int j = 0; j < animations.length; j++) 
-                    for (int i = 0; i < 11; i++)
-                       animations[j][i] = sprite.getSubimage(i*22, j*24, 22, 24);
+	public void update() {
+		updatePos();
+		updateAnimationTick();
+		setAnimation();
+	}
 
-        } catch (IOException ex) {
-            Logger.getLogger(GamePanel.class.getName()).log(Level.SEVERE, null, ex);
-        } finally{
-            try{
-                is.close();
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-        }
-        
-        
-    }
+	public void render(Graphics g) {
+		g.drawImage(animations[playerAction][aniIndex], (int) x, (int) y, 100, 100, null);
+	}
 
-    public boolean isLeft() {
-        return left;
-    }
+	private void updateAnimationTick() {
+		aniTick++;
+		if (aniTick >= aniSpeed) {
+			aniTick = 0;
+			aniIndex++;
+			if (aniIndex >= GetSpriteAmount(playerAction)) {
+				aniIndex = 0;
+				attacking = false;
+			}
 
-    public void setLeft(boolean left) {
-        this.left = left;
-    }
+		}
 
-    public boolean isUp() {
-        return up;
-    }
+	}
 
-    public void setUp(boolean up) {
-        this.up = up;
-    }
+	private void setAnimation() {
+		int startAni = playerAction;
 
-    public boolean isRight() {
-        return right;
-    }
+		if (moving)
+			playerAction = WALK;
+		else
+			playerAction = IDLE;
 
-    public void setRight(boolean right) {
-        this.right = right;
-    }
+		if (attacking)
+			playerAction = SWORD;
 
-    public boolean isDown() {
-        return down;
-    }
+		if (startAni != playerAction)
+			resetAniTick();
+	}
 
-    public void setDown(boolean down) {
-        this.down = down;
-    }
-    
+	private void resetAniTick() {
+		aniTick = 0;
+		aniIndex = 0;
+	}
+
+	private void updatePos() {
+		moving = false;
+
+		if (left && !right) {
+			x -= playerSpeed;
+			moving = true;
+		} else if (right && !left) {
+			x += playerSpeed;
+			moving = true;
+		}
+
+		if (up && !down) {
+			y -= playerSpeed;
+			moving = true;
+		} else if (down && !up) {
+			y += playerSpeed;
+			moving = true;
+		}
+	}
+
+	private void loadAnimations() {
+		InputStream is = getClass().getResourceAsStream("/SpriteSheet.png");
+		try {
+			BufferedImage img = ImageIO.read(is);
+
+			animations = new BufferedImage[6][11];
+			for (int j = 0; j < animations.length; j++)
+				for (int i = 0; i < animations[j].length; i++)
+					animations[j][i] = img.getSubimage(i * 22, j * 24, 22, 24);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void resetDirBooleans() {
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+	}
+
+	public void setAttacking(boolean attacking) {
+		this.attacking = attacking;
+	}
+
+	public boolean isLeft() {
+		return left;
+	}
+
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+
+	public boolean isUp() {
+		return up;
+	}
+
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+
+	public boolean isRight() {
+		return right;
+	}
+
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
+	public boolean isDown() {
+		return down;
+	}
+
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+
 }
-
-
--- DarksidersSurvivors/src/view/KeyboardInputs.java
+++ DarksidersSurvivors/src/view/KeyboardInputs.java
@@ -1,61 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package view;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
+import view.GamePanel;
 import static model.Constants.Directions.*;
-/**
- *
- * Program created by
- * @author Anthony Alvarez Delgado
- * Software Engeneer Student - UIA
- *
- */
+
 public class KeyboardInputs implements KeyListener {
 
-    private GamePanel gamePanel;
-    public KeyboardInputs (GamePanel gamePanel){
-        this.gamePanel = gamePanel;
-    }
-    
-    
-    public void keyTyped(KeyEvent e) {
-        
-    }
+	private GamePanel gamePanel;
 
-     public void keyReleased(KeyEvent e) {
-        switch(e.getKeyCode()){
-            case KeyEvent.VK_W:
-                gamePanel.getGame().getPlayer().setUp(false);
-            case KeyEvent.VK_A:
-                gamePanel.getGame().getPlayer().setLeft(false);
-            case KeyEvent.VK_S:
-                gamePanel.getGame().getPlayer().setDown(false);
-            case KeyEvent.VK_D:
-                gamePanel.getGame().getPlayer().setRight(false);
-                break;
-        }
-    }
+	public KeyboardInputs(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
 
-    public void keyPressed(KeyEvent e) {
-        switch(e.getKeyCode()){
-            case KeyEvent.VK_W:
-                gamePanel.getGame().getPlayer().setUp(true);
-            case KeyEvent.VK_A:
-                gamePanel.getGame().getPlayer().setLeft(true);
-            case KeyEvent.VK_S:
-                gamePanel.getGame().getPlayer().setDown(true);
-            case KeyEvent.VK_D:
-                gamePanel.getGame().getPlayer().setRight(true);
-                break;
-        }
-    }
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
 
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			gamePanel.getGame().getPlayer().setUp(false);
+			break;
+		case KeyEvent.VK_A:
+			gamePanel.getGame().getPlayer().setLeft(false);
+			break;
+		case KeyEvent.VK_S:
+			gamePanel.getGame().getPlayer().setDown(false);
+			break;
+		case KeyEvent.VK_D:
+			gamePanel.getGame().getPlayer().setRight(false);
+			break;
+		}
+	}
 
-   
-
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			gamePanel.getGame().getPlayer().setUp(true);
+			break;
+		case KeyEvent.VK_A:
+			gamePanel.getGame().getPlayer().setLeft(true);
+			break;
+		case KeyEvent.VK_S:
+			gamePanel.getGame().getPlayer().setDown(true);
+			break;
+		case KeyEvent.VK_D:
+			gamePanel.getGame().getPlayer().setRight(true);
+			break;
+		}
+	}
 }
