b4cdc990ffc845bcb7d41ee835f50b30ace4ce21
Anthony Alvarez Delgado <107961797+aalvarez-d@users.noreply.github.com>
2023-04-03 17:11:35 -0600

    Update KeyboardInputs.java

--- DarksidersSurvivors/src/view/KeyboardInputs.java
+++ DarksidersSurvivors/src/view/KeyboardInputs.java
@@ -23,15 +23,19 @@ public class KeyboardInputs implements KeyListener {
     public void keyReleased(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
+            case KeyEvent.VK_UP:
                 gamePanel.getGame().getPlayer().setUp(false);
                 break;
             case KeyEvent.VK_A:
+            case KeyEvent.VK_LEFT:
                 gamePanel.getGame().getPlayer().setLeft(false);
                 break;
             case KeyEvent.VK_S:
+            case KeyEvent.VK_DOWN:
                 gamePanel.getGame().getPlayer().setDown(false);
                 break;
             case KeyEvent.VK_D:
+            case KeyEvent.VK_RIGHT:
                 gamePanel.getGame().getPlayer().setRight(false);
                 break;
 
@@ -42,15 +46,19 @@ public class KeyboardInputs implements KeyListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
+            case KeyEvent.VK_UP:
                     gamePanel.getGame().getPlayer().setUp(true);
                     break;
             case KeyEvent.VK_A:
+            case KeyEvent.VK_LEFT:
                     gamePanel.getGame().getPlayer().setLeft(true);
                     break;
             case KeyEvent.VK_S:
+            case KeyEvent.VK_DOWN:
                     gamePanel.getGame().getPlayer().setDown(true);
                     break;
             case KeyEvent.VK_D:
+            case KeyEvent.VK_RIGHT:
                     gamePanel.getGame().getPlayer().setRight(true);
                     break;
             case KeyEvent.VK_1:
